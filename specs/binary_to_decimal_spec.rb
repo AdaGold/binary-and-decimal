30476e6d19260ad8f53027448db9adeb0e65b544
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-19 22:36:46 -0700

    First commit

--- specs/binary_to_decimal_spec.rb
+++ specs/binary_to_decimal_spec.rb
@@ -1,5 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
+require "minitest/skip_dsl"
+require "minitest/pride"
+require "pry"
+require "awesome_print"
 require_relative '../lib/binary_to_decimal'
 
 describe "binary to decimal" do
