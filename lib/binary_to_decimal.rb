fafb7b19c19f4b1fb90fb35eb1ef4bec0305295d
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-22 21:27:04 -0700

    created binary to decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,17 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  raise NotImplementedError
+def binary_to_decimal(binary_number)
+  binary_number_array = binary_number.split('')
+  decimal = 0
+    j = 7
+  binary_number_array.each do |digit|
+      product = digit.to_i * (2**j)
+      j -= 1
+      decimal = decimal.to_i + product.to_i
+  end
+  return decimal
 end
+
+
+p binary_to_decimal('11110100')
