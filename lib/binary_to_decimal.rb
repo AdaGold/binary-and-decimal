4c59b5512fc64eeba1b9ffd548c8db482afcce39
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-02-22 12:18:05 -0800

    Write code inside the binary_to_decimal method to make tests pass

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,23 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+# binary_array = [1, 0, 0, 1, 1, 0, 0, 1]
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  # raise NotImplementedError
+  power = binary_array.length - 1
+  base_10 = 0
+
+  binary_array.each do |i|
+    if i == 1
+      base_10 += 2 ** power
+    end
+    power -= 1
+  end
+  puts "#{binary_array} in base 10 is: #{base_10}"
+  return base_10
 end
+
+binary_to_decimal([1, 0, 0, 1, 1, 0, 0, 1])
+binary_to_decimal([0, 0, 0, 0, 1, 1, 0, 1])
+binary_to_decimal([1, 0, 0, 0, 0, 0, 0, 0])
